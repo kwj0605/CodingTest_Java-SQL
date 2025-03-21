2b3a8f21c97111e48a009880a6d77bf281da8738
김우진 <107970778+kwj0605@users.noreply.github.com>
2025-03-21 12:35:29 +0900

    [level 2] Title: 소수 찾기, Time: 30.35 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 83.8 MB, 시간: 26.67 ms
+메모리: 90.2 MB, 시간: 30.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 21일 12:20:35
+2025년 03월 21일 12:35:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,43 +1,37 @@
 import java.util.*;
 class Solution {
-    HashSet<Integer> numberSet = new HashSet<>();
-    public boolean isPrime(int num) {
+    HashSet<Integer> numbersSet = new HashSet<>();
+    void recur(String comp, String nums) {
+        if(!comp.equals("")) {
+            numbersSet.add(Integer.valueOf(comp));
+        }
+        
+        for(int i=0; i<nums.length(); i++) {
+            recur(comp+nums.charAt(i), nums.substring(0, i)+nums.substring(i+1));
+        }
+    }
+    boolean isPrime(int num) {
         if(num == 0 || num == 1) return false;
         
         int lim = (int)Math.sqrt(num);
         
         for(int i=2; i<=lim; i++) {
-            if(num%i == 0) {
-                return false;
-            }
+            if(num%i == 0) return false;
         }
         return true;
     }
-    public void recur(String comb, String numbers) {
-        // 현재 조합을 numberSet에 저장한다.
-        if(!comb.equals(""))
-            numberSet.add(Integer.valueOf(comb));
-    
-        // 남은 숫자 중에서 한 개를 더 해 새로운 조합을 만든다.
-        for(int i=0; i<numbers.length(); i++) {
-            recur(comb+numbers.charAt(i), numbers.substring(0, i)+numbers.substring(i+1));
-        }
-    }
     public int solution(String numbers) {
         int answer = 0;
-        
-        // 한문자씩 숫자 배열 생성
+        // 모든 숫자 배열 생성
         recur("", numbers);
+        // 소수 인지 판단
+        Iterator<Integer> it = numbersSet.iterator();
         
-        // 숫자 하나씩 가져와서 소수 판단
-        Iterator<Integer> it = numberSet.iterator();
         while(it.hasNext()) {
             int num = it.next();
-            if(isPrime(num)) {
+            if(isPrime(num))
                 answer++;
-            }
         }
-        // 소수 개수 반환
         
         return answer;
     }
