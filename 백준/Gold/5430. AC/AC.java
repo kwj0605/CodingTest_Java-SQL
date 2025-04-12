57bc219d770c773e1ad1b0df9b915450eb70f974
김우진 <107970778+kwj0605@users.noreply.github.com>
2025-04-12 12:10:09 +0900

    [Gold V] Title: AC, Time: 964 ms, Memory: 104772 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,62 +1,73 @@
 import java.util.*;
 
 public class Main {
+    static List<String> p = new ArrayList<>(); // 수행할 함수
+    static List<Deque<Integer>> num = new ArrayList<>(); // 정수형 리스트
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt(); // 테스트 케이스 개수
 
-        for (int t = 0; t < T; t++) {
-            String func = sc.next(); // 수행할 함수
-            int n = sc.nextInt(); // 배열 크기
-            String arrS = sc.next(); // 배열 입력값 (예: "[1,2,3,4]")
+        int T = sc.nextInt();
+        for(int i=0; i<T; i++) {
+            String func = sc.next();
+            int n = sc.nextInt();
+            String arrS = sc.next(); // [x1, ..., xn] 형태
+            Deque<Integer> nums = new LinkedList<>();
 
-            // 덱(Deque) 초기화
-            Deque<Integer> deque = new LinkedList<>();
-            if (n > 0) {
-                // 배열 파싱 (대괄호 제거 후 "," 기준으로 분리)
-                String[] elements = arrS.substring(1, arrS.length() - 1).split(",");
-                for (String element : elements) {
-                    deque.add(Integer.parseInt(element));
+            if (n != 0) {
+                String[] elements = arrS.substring(1, arrS.length()-1).split(",");
+                for(String element : elements) {
+                    nums.add(Integer.parseInt(element));
                 }
             }
+            // List에 넣기
+            p.add(func);
+            num.add(nums);
+        }
+        sc.close();
 
-            boolean rev = false; // 뒤집기 상태 플래그
-            boolean error = false; // 에러 플래그
-
-            for (char command : func.toCharArray()) {
-                if (command == 'R') {
-                    rev = !rev; // 뒤집기 상태 변경
-                } else { // 'D' 명령어
-                    if (deque.isEmpty()) { // 배열이 비어있는 경우
-                        error = true;
-                        break;
-                    }
-                    if (rev) {
-                        deque.removeLast(); // 뒤집힌 상태면 뒤에서 삭제
-                    } else {
-                        deque.removeFirst(); // 일반 상태면 앞에서 삭제
+        for(int i=0; i<T; i++) {
+            String function = p.get(i);
+            Deque<Integer> nums = num.get(i);
+            boolean rev = false;
+            boolean error = false;
+            for(int j=0; j<function.length(); j++) {
+                if(function.charAt(j)=='R') { // 명령어 R
+                    rev = !rev;
+                } else { // 명령어 D
+                    if(nums.isEmpty()) {
+                        error = true; // error 로 error 출력
+                        break; // ** -1이 들어있는 상태로 또 D를 수행하면 -1이 사라짐
+                    } else{
+                        if(rev) {
+                            nums.removeLast();
+                        } else {
+                            nums.removeFirst();
+                        }
                     }
                 }
             }
 
             // 출력
-            if (error) {
+            if(error) {
                 System.out.println("error");
-            } else {
-                // 결과 출력
+            } else{
                 StringBuilder sb = new StringBuilder();
                 sb.append("[");
-
-                while (!deque.isEmpty()) {
-                    sb.append(rev ? deque.removeLast() : deque.removeFirst());
-                    if (!deque.isEmpty()) sb.append(",");
+                while(!nums.isEmpty()) {
+                    sb.append(rev ? nums.removeLast() : nums.removeFirst());
+                    if(!nums.isEmpty()) {
+                        sb.append(",");
+                    }
                 }
-
                 sb.append("]");
                 System.out.println(sb);
             }
         }
-
-        sc.close();
     }
 }
+
+// 1. reverse는 Collections.reverse()로 구현하기 위해 List
+// 2. drop은 앞의 수를 뺄 수 있는 queue로 구현
+// 시간 초과 -> dequeue를 사용해서 reverse하고 drop이면 뒤에서 빼고 아니면 앞에서 빼고 | 직접 reverse 연산을 하지 않음.
+
