5ca63139c1740f780b868259559fecf18234f7d7
김우진 <107970778+kwj0605@users.noreply.github.com>
2025-05-01 17:09:58 +0900

    [level 3] Title: 봉인된 주문, Time: 485.05 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
@@ -4,40 +4,47 @@ class Solution {
     public String solution(long n, String[] bans) {
         String answer = "";
         
-        Queue<String> bansQueue = new ArrayDeque<>();
+        Queue<String> banQ = new ArrayDeque<>();    // 밴할 내용들 저장
+        
         Arrays.sort(bans, (o1, o2) -> {
-            if(o1.length() == o2.length()) return o1.compareTo(o2);
-            return o1.length() - o2.length();
+            if(o1.length() == o2.length()) return o1.compareTo(o2); // 길이가 같으면 문자 오름차순
+            return o1.length() - o2.length();   // 길이 다르면 길이가 작은 순서
         });
-        for(int i=0; i<bans.length; i++) {
-            bansQueue.add(bans[i]);
+        
+        // 밴할 주문 저장
+        for(String ban : bans) {
+            banQ.offer(ban);
         }
         
-        while(!bansQueue.isEmpty()) {
-            String cur = bansQueue.peek();
-            String target = getString(n);
+        while(!banQ.isEmpty()) {
+            String cur = banQ.peek();
+            String target = getString(n); // n 번째 주문 찾기
+            
+            // 봉인된 주문이 n번째 주문보다 길이가 작거나, 문자가 앞에 있을 때
             if(cur.length() < target.length() || (cur.length() == target.length() && cur.compareTo(target) <= 0)) {
-                bansQueue.poll();
-                n++;
+                banQ.poll(); // 사용된 금지된 주문은 빼낸다.
+                n++; // 그 다음 문자를 찾음
             } else {
                 break;
             }
         }
+        
         return getString(n);
     }
     
     private String getString(long n) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); // 주문을 만들 빌더
         while(n>0) {
-            long remained = n%26;
+            long remained = n%26;   // 알파벳이 26자이므로 26씩 나눔
             n /= 26;
             if(remained == 0) {
-                n--;
                 sb.append('z');
+                n--;
             } else {
-                sb.append((char)('a' + remained - 1));
+                sb.append((char)('a'+ remained - 1));   // a에서 나머지만큼 더해서 해당 영어의 숫자
             }
         }
-        return sb.reverse().toString();
+        
+        return sb.reverse().toString(); // 뒤에서부터 주문을 만들어 나갔으니, reverse 필요
     }
 }
