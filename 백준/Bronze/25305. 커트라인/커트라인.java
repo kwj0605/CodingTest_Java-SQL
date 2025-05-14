acf39d4b302bedd19fece1ca9b5ef894ce1a7030
김우진 <107970778+kwj0605@users.noreply.github.com>
2025-05-14 23:16:10 +0900

    [Bronze II] Title: 커트라인, Time: 224 ms, Memory: 20172 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -8,14 +8,21 @@ public class Main {
         int N = sc.nextInt();
         int k = sc.nextInt();
 
-        // 내림차순을 comparator 사용하기 위해 객체로 생성
-        Integer[] score = new Integer[N];
+        int[] score = new int[N];
 
         for(int i=0; i<N; i++) {
             score[i] = sc.nextInt();
         }
 
-        Arrays.sort(score, (a, b) -> b - a);
+        Arrays.sort(score);
+
+        // 뒤집기
+        for(int i=0; i<N/2; i++) {
+            int temp = score[i];
+            score[i] = score[N-1-i];
+            score[N-1-i] = temp;
+        }
+        
         System.out.println(score[k-1]);
     }
 }
