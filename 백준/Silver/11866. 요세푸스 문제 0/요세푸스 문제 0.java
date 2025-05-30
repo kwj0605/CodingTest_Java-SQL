db959e8775bb88c0754f3330a6cef12afd7145c2
김우진 <107970778+kwj0605@users.noreply.github.com>
2025-05-30 23:14:42 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,41 +1,32 @@
 import java.util.*;
 
 public class Main {
- 
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		Queue<Integer> q = new LinkedList<>();
-		
-		int N = in.nextInt();
-		int K = in.nextInt();
-		
-		
-		for(int i = 1; i <= N; i++) {
-			q.add(i);
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append('<');
-		
-		/*
-		 *  마지막 부분의 출력은 > 괄호 전에 공백이 없기 때문에
-		 *  일괄적으로 출력하기 위해 마지막 원소만 남겨질 때까지만
-		 *  반복하고 마지막 원소는 그대로 출력한다.
-		 */
-		while(q.size() > 1) {
-			
-			for(int i = 0; i < K - 1; i++) {
-				int val = q.poll();
-				q.offer(val);
-			}
-			
-			sb.append(q.poll()).append(", ");
-		}
- 
-		// 마지막 원소 출력한 뒤 > 도 붙여준다.
-		sb.append(q.poll()).append('>');
-		System.out.println(sb);
-	}
- 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // list 사용
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<");
+
+        int idx = 0;
+        while (list.size() > 1) {
+            idx = (idx + (K - 1)) % list.size();
+            sb.append(list.get(idx) + ", ");
+            list.remove(idx);
+        }
+
+        sb.append(list.get(0) + ">");
+
+        System.out.println(sb);
+    }
 }
