760df6725137af02b350709d22ee55842a0d7676
김우진 <107970778+kwj0605@users.noreply.github.com>
2025-06-05 08:23:25 +0900

    [Silver V] Title: 국회의원 선거, Time: 172 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -1,12 +1,26 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int me = sc.nextInt(); //나 == 다솜
+		Integer arr[] = new Integer[N-1]; //내림차순 정렬을 위해 박싱
+		int count = 0; //매수해야 하는 사람 수
+		for(int i = 0; i < arr.length; ++i) arr[i] = sc.nextInt();
+		while(true)
+		{
+			Arrays.sort(arr, Collections.reverseOrder()); //내림차순 정렬
+			if(N == 1 || arr[0] < me) break; //N이 0이거나 배열의 가장 큰 수가 나보다 작으면 탈출
+			else
+			{
+				++me;
+				--arr[0];
+				++count;
+			}
+		}
+		System.out.print(count);
+	}
+}
