2069f32363170a8f1f344cb1c811b3046ca6b144
김우진 <107970778+kwj0605@users.noreply.github.com>
2025-06-15 20:07:25 +0900

    [Silver III] Title: N과 M (1), Time: 1968 ms, Memory: 66900 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static boolean[] used;
+    static int[] result;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        used = new boolean[N + 1];
+        result = new int[M + 1];
+
+        backtrack(0);
+    }
+
+    public static void backtrack(int depth) {
+        // M과 같으면 출력
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        // 유망한지 확인(사용되지 X)
+        for (int i = 1; i <= N; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                result[depth] = i;
+
+                backtrack(depth + 1);
+
+                used[i] = false;
+            }
+        }
+    }
+}
