704f902de265eabd42f5ce5ea2d36e9cbf322f62
김우진 <107970778+kwj0605@users.noreply.github.com>
2025-07-09 00:40:51 +0900

    [Silver IV] Title: 문자열 집합, Time: 940 ms, Memory: 69040 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -8,18 +8,15 @@ public class Main {
         int M = sc.nextInt();
         int result = 0;
 
-        HashMap<Integer, String> map = new HashMap();
+        HashSet<String> set = new HashSet();
 
         for (int i = 0; i < N; i++) {
-            map.put(i, sc.next());
+            set.add(sc.next());
         }
 
         for (int i = 0; i < M; i++) {
-            String arr = sc.next();
-            for (int j = 0; j < N; j++) {
-                if (map.get(j).equals(arr)) {
-                    result++;
-                }
+            if (set.contains(sc.next())) {
+                result++;
             }
         }
 
