b170cb65763225ff2b15b7f943d8c706b75ac557
김우진 <107970778+kwj0605@users.noreply.github.com>
2025-08-22 07:53:28 +0900

    [level 2] Title: 과제 진행하기, Time: 10.10 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -1,68 +1,63 @@
 import java.util.*;
 
+class Task {
+    String name;
+    int start;
+    int left;
+    
+    Task(String name, int start, int left) {
+        this.name = name;
+        this.start = start;
+        this.left = left;
+    }
+}
+
 class Solution {
+    private int convertToMinute(String minute) {
+        String[] time = minute.split(":");
+        return Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
+    }
+    
     public String[] solution(String[][] plans) {
-        
-        Queue<Task> tasks = new PriorityQueue<>((t1, t2) -> t1.start - t2.start);
+        LinkedList<Task> tasks = new LinkedList<>();
         for(String[] plan : plans) {
             tasks.offer(new Task(plan[0], convertToMinute(plan[1]), Integer.parseInt(plan[2])));
         }
+        // 시작시간별 정렬
+        tasks.sort((t1, t2) -> t1.start - t2.start);
         
+        // 남은 과제는 Stack에 저장
+        // 다한 과제는 List에 저장
         Stack<Task> stopTask = new Stack<>();
         List<String> endTask = new ArrayList<>();
         
         Task now = tasks.poll();
-        int time = now.start + now.pt;
-        
+        int time = now.start + now.left;
         while(!tasks.isEmpty()) {
             Task next = tasks.peek();
             
             if(time > next.start) {
-                now.pt = time - next.start;
+                now.left = time - next.start;
                 stopTask.push(now);
             } else {
                 endTask.add(now.name);
                 if(!stopTask.isEmpty()) {
                     now = stopTask.pop();
-                    time += now.pt;
+                    time = time + now.left; // 시간은 현재 과제가 끝난 시간 + 멈춘 과제의 해야할 시간
                     continue;
                 }
             }
             
             now = tasks.poll();
-            time = now.start + now.pt;
+            time = now.start + now.left;
         }
         
         endTask.add(now.name);
+        
         while(!stopTask.isEmpty()) {
-            now = stopTask.pop();
-            endTask.add(now.name);
+            endTask.add(stopTask.pop().name);
         }
         
         return endTask.toArray(new String[0]);
     }
-    
-    private int convertToMinute(String start) {
-        String[] time = start.split(":");
-        return Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
-    }
-    
-    class Task {
-        String name;
-        int start;
-        int pt;
-        
-        public Task(String name, int start, int pt) {
-            this.name = name;
-            this.start = start;
-            this.pt = pt;
-        }
-    }
 }
-
-// 1. Task 클래스로 객체 정의
-// 2. Task를 Queue에 넣어서 start 별로 오름차순 정의
-// 3. 다음과제의 start < 현재과제의 start + playtime : 남은 시간은 현제 과제의 playtime 저장
-// 4. 멈춘 과제는 stack에 저장
-// 5. 다음과제의 start >= 현재과제의 start + playtime : 멈춘 과제가 있으면 가져오고 다시 3번으로 시작.
-// 6. 끝난 과제는 List에 저장
