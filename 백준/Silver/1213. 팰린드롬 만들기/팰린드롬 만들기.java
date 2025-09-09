fc6bf12ee8cf25a17481a42cfc586e866aceb417
김우진 <107970778+kwj0605@users.noreply.github.com>
2025-09-09 10:05:44 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 168 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -5,27 +5,27 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String s = sc.next();
+        String s = sc.next(); 
         sc.close();
 
-        int[] alp = new int[26];
-
         StringBuilder sb = new StringBuilder();
-        StringBuilder start = new StringBuilder();
+        StringBuilder front = new StringBuilder();
         char mid = ' ';
         StringBuilder end = new StringBuilder();
 
-        // 알파벳 갯수 구하기
-        for (int i = 0; i < s.length(); i++) {
-            alp[s.charAt(i) - 65]++;
+        int[] alp = new int[26];
+
+        int oddCheck = 0; // 홀수 갯수
+        boolean check = false; // 팰린드롬이 가능한지
+
+        for(int i=0; i<s.length(); i++) {
+            alp[s.charAt(i)-65]++;
         }
 
-        // 팰린드롬 만들기
-        boolean check = false; // 팰린드롬 만들 수 있는 지 확인
-        int oddCheck = 0; // 홀수 갯수 체크
-        for (int i = 0; i < 26; i++) {
-            if (alp[i] != 0 && alp[i] % 2 == 1) {
-                if (oddCheck == 0) {
+        for(int i=0; i<26; i++) {
+            // 홀수
+            if(alp[i] != 0 && alp[i]%2 == 1) {
+                if(oddCheck == 0) {
                     mid = (char) ('A' + i);
                     oddCheck++;
                 } else {
@@ -34,20 +34,23 @@ class Main {
                     break;
                 }
             }
-            for (int j = 0; j < alp[i] / 2; j++) {
-                char a = (char) ('A' + i);
-                start.append(a);
-                end.insert(0, a);
-            }
-        }
-        if (!check) {
-            if (mid == ' ') {
-                sb.append(start).append(end);
-            } else {
-                sb.append(start).append(mid).append(end);
+
+            if(alp[i] != 0) {
+                for(int j=0; j<alp[i]/2; j++) {
+                    char c = (char) ('A'+ i);
+                    front.append(c);
+                    end.insert(0, c);
+                }
             }
         }
 
+        if(!check) {
+            if(mid == ' ') {
+                sb.append(front).append(end);
+            } else {
+                sb.append(front).append(mid).append(end);
+            }
+        }
         System.out.println(sb);
     }
 }
